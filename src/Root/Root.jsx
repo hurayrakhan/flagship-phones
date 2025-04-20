265886c8e3d5751328b1348fb2782bf290b22008
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-20 22:17:01 +0600

    added navbar

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Navbar from '../Components/Navbar/navbar';
+import { Outlet } from 'react-router';
+import Footer from '../Components/Footer/Footer';
 
 const Root = () => {
     return (
-        <div>
-            i am root
-        </div>
+        <>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </>
     );
 };
 
