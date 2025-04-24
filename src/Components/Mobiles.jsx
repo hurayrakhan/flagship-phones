8f07b5a10ae2b282644f8200b3254a000779d040
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-24 23:27:07 +0600

    added phones card and show details button and apply all styles

--- src/Components/Mobiles.jsx
+++ src/Components/Mobiles.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
-const Mobiles = () => {
+const Mobiles = ({ phone }) => {
 
+    const { name, brand, description, image } = phone;
 
-    
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card bg-base-100 border shadow-md border-gray-400  shadow-sm">
             <figure>
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes" />
+                <img className=''
+                    src={image}>
+                </img>
             </figure>
             <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <h2 className="card-title">{name}</h2>
+                <p>{description}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <a href="#_" class="relative inline-block px-4 py-2 font-medium group">
+                        <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                        <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                        <span class="relative text-black group-hover:text-white">Show Details</span>
+                    </a>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-10/12 mx-auto my-20'> 
                 {
                     phonesData.map(phone => <Mobiles phone={phone} key={phone.id}></Mobiles>)
                 }
