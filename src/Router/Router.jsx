d7757c52767527a0a0285c5ef25083eda6bd7755
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-26 22:32:14 +0600

    added errorpage when load a unknown route

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,16 +4,26 @@ import Home from "../Pages/Home/Home";
 import About from "../Pages/About/About";
 import Cart from "../Pages/Cart/Cart";
 import Favorites from "../Pages/Favorites/Favorites";
+import ErrorPage from "../Pages/ErrorPage.Jsx";
+import Navbar from "../Components/Navbar/navbar.jsx";
+
 
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root></Root>,
+       errorElement: <div>
+        <Navbar></Navbar>
+        <ErrorPage></ErrorPage>
+        
+       </div>,
+        
         children: [
             {
                 path: '/',
                 element: <Home></Home>,
+                
                 loader: () => fetch('/phones.json'),
             },
             {
