2f6f6038675287f3e9f2e9c3cd18c8456cb2f026
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-26 22:41:46 +0600

    modified errorpage and added styles

--- src/Components/Mobiles.jsx
+++ src/Components/Mobiles.jsx
@@ -15,10 +15,10 @@ const Mobiles = ({ phone }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{description}</p>
                 <div className="card-actions justify-end">
-                    <a href="#_" class="relative inline-block px-4 py-2 font-medium group">
-                        <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                        <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                        <span class="relative text-black group-hover:text-white">Show Details</span>
+                    <a href="#_" className="relative inline-block px-4 py-2 font-medium group">
+                        <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                        <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                        <span className="relative text-black group-hover:text-white">Show Details</span>
                     </a>
                 </div>
             </div>
